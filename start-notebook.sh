80d6ae25ca71ae73a458c806bb35534d571963b1
Mesut Karakoç <mkarakoc@users.noreply.github.com>
2017-11-23 23:56:52 +0300

    Update start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -9,4 +9,4 @@ if [ -e $HOME/.binder_start ]; then
 fi
 CMD="$OPTS $@"
 echo "CMD: " $CMD
-jupyter notebook --NotebookApp.iopub_data_rate_limit=10000000000 $CMD
+jupyter notebook --NotebookApp.iopub_data_rate_limit=1.0e11 $CMD
